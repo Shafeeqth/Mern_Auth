a994e6b287ab416521ec8b4dfb2fffb2300a9848
shafeeque <shafeeqsha06@gmail.com>
2024-09-12 14:34:07 +0530

    feat(asynchandler): implemented the built in asynchandler

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,11 @@
-const authUser = (req, res) => {
-    res.status().json
+import asyncHandler from 'express-async-handler';
+
+/**
+ * @desc Auth user/set token 
+ * route POST /api/users/auth
+ * @access Public
+ */
+export const authUser = (req, res) => {
+    res.status(200).json({message: "Auth User"});
 
 }
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-
 const port = process.env.PORT || 5000;
+import userRoute from './routes/userRoutes.js';
 
 const app = express();
+app.use('/api/user', userRoute)
 
 app.listen(port, () => {
-    console.log('server running on http://localhost:'+port );
+    console.log('server running on http://localhost:' + port);
 })
 
