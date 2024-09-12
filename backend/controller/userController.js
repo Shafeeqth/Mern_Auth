367fc46e66965a0be69c7e4f8aba31c1e73d10c0
shafeeque <shafeeqsha06@gmail.com>
2024-09-12 15:05:21 +0530

    feat(useRoute): connected all basic user routes with controllers

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -5,7 +5,48 @@ import asyncHandler from 'express-async-handler';
  * route POST /api/users/auth
  * @access Public
  */
-export const authUser = (req, res) => {
+export const authUser = asyncHandler((req, res) => {
+   
     res.status(200).json({message: "Auth User"});
 
-}
+});
+/**
+ * @desc Register user
+ * route POST /api/users
+ * @access Public
+ */
+export const registerUser = asyncHandler((req, res) => {
+   
+    res.status(200).json({message: "Register User"});
+
+});
+/**
+ * @desc Logout user
+ * route GET /api/users/logout
+ * @access Public
+ */
+export const logoutUser = asyncHandler((req, res) => {
+   
+    res.status(200).json({message: "logout User"});
+
+});
+/**
+ * @desc Get user profile 
+ * route GET /api/users/auth
+ * @access Private
+ */
+export const getUserProfile = asyncHandler((req, res) => {
+   
+    res.status(200).json({message: "User User"});
+
+});
+/**
+ * @desc Update user profile 
+ * route Put /api/users/auth
+ * @access Private
+ */
+export const updateUserProfile = asyncHandler((req, res) => {
+   
+    res.status(200).json({message: "Update user profile"});
+
+});
--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,14 @@ import dotenv from "dotenv";
 dotenv.config();
 const port = process.env.PORT || 5000;
 import userRoute from './routes/userRoutes.js';
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
-app.use('/api/user', userRoute)
+app.use('/api/user', userRoute);
+
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log('server running on http://localhost:' + port);
