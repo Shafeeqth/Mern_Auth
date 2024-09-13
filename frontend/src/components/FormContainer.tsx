245d4f14410eae22221a974670007939d36b15ca
shafeeque <shafeeqsha06@gmail.com>
2024-09-13 15:45:56 +0530

    refactor(store): added store dir maintain redux store concerns

--- frontend/src/components/FormContainer.tsx
+++ frontend/src/components/FormContainer.tsx
@@ -6,7 +6,7 @@ const FormContainer = ({ children }) => {
     <Container>
       <Row className="justify-content-md-center mt-t">
         <Col xs={12} md={6} className="card p-5">
-        {children}
+          {children}
         </Col>
       </Row>
     </Container>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,15 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen.jsx';
+import LoginScreen from './screens/LoginScreens.jsx';
+import SignupScreen from './screens/SignupScreen.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App/>}>
       <Route index={true} path='/' element={<HomeScreen/>}/>
+      <Route path='/login' element={ <LoginScreen/> }/>
+      <Route path='/register' element={ <SignupScreen/>}/>
     </Route>
   )
 )
