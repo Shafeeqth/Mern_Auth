91b6743e5b2fbd7acc070ce5d8349da4f02bcbf0
shafeeque <shafeeqsha06@gmail.com>
2024-09-13 16:51:30 +0530

    feat(reduxt/apiSlice):implemented redux api

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen.jsx';
-import LoginScreen from './screens/LoginScreens.jsx';
-import SignupScreen from './screens/SignupScreen.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen.jsx";
+import LoginScreen from "./screens/LoginScreens.jsx";
+import SignupScreen from "./screens/SignupScreen.jsx";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App/>}>
-      <Route index={true} path='/' element={<HomeScreen/>}/>
-      <Route path='/login' element={ <LoginScreen/> }/>
-      <Route path='/register' element={ <SignupScreen/>}/>
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<SignupScreen />} />
     </Route>
   )
-)
+);
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </Provider>
+);
