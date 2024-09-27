e18c66ccb7a2969a0b670eccde25a0acc61409dd
shafeeque <shafeeqsha06@gmail.com>
2024-09-27 21:57:31 +0530

    chore: added and updated some files and dipendencies

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,17 +1,24 @@
 import jwt from 'jsonwebtoken';
 
  const generateToken = (res, userId) => {
-    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
-        expiresIn: '1d'
-    });
+    try {
+        const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+            expiresIn: '1d',
+            issuer: 'shafeeq.com'
+        });
+        
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== 'development',
+            sameSite: 'strict', 
+            maxAge: 1 * 24 * 60 * 60 * 1000
     
-    res.cookie('jwt', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'strict', 
-        maxAge: 1 * 24 * 60 * 60 * 1000
-
-    })
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({success: false, error: true, message: "Something went wrong"})
+        
+    }
  }
 
  export default generateToken;
