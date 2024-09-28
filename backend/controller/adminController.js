12d7a5c546c3b06026e912a8bd71d60c4feb43ea
shafeeque <shafeeqsha06@gmail.com>
2024-09-28 08:15:13 +0530

    feat(adminController): set up various admin api endpoints logic

--- backend/controller/adminController.js
+++ backend/controller/adminController.js
@@ -9,18 +9,18 @@ import cloudinary from "../utils/setupCloudinary";
  * @access public
  */
 export const authAdmin = asyncHandler(async (req, res) => {
-    const {email, password} = req.body;
-    const admin = await User.findOne({email});
-    if(!admin || !admin?.isAdmin) {
+    const { email, password } = req.body;
+    const admin = await User.findOne({ email });
+    if (!admin || !admin?.isAdmin) {
         return res.status(403)
-        .json({
-            success: false,
-            error: false,
-            message: "Invalid admin Email Id"
-        })
+            .json({
+                success: false,
+                error: false,
+                message: "Invalid admin Email Id"
+            })
     }
     const isPasswordMatch = await admin.matchPasswords(password);
-    if(!isPasswordMatch) {
+    if (!isPasswordMatch) {
         return res.status(401).json({
             error: true,
             success: false,
@@ -28,34 +28,41 @@ export const authAdmin = asyncHandler(async (req, res) => {
         })
     }
     generateToken(res, admin._id, 'adminJWT')
-    return  res.status(200).json({
+    return res.status(200).json({
         _id: admin._id,
         name: admin.name,
         email: admin.email,
     })
 });
 
+
+/**
+ * @desc Create users
+ * @method POST api/admin/create-user
+ * @access Private
+ */
+
 const createUser = asyncHandler(async (req, res) => {
-    const {name, email, password} = req.body;
-    if(!name || !password || !email) {
-       return res.status(400)
-        .json({
-            error: true,
-            success: false,
-            message: "All feilds are required!"
-        })
-    }
-    const alreadyAUser = await User.findOne({email});
-    if(alreadyAUser) {
+    const { name, email, password } = req.body;
+    if (!name || !password || !email) {
         return res.status(400)
-        .json({
-            error: true,
-            success: false,
-            message: 'Email already exist!',
-        })
+            .json({
+                error: true,
+                success: false,
+                message: "All feilds are required!"
+            })
+    }
+    const alreadyAUser = await User.findOne({ email });
+    if (alreadyAUser) {
+        return res.status(400)
+            .json({
+                error: true,
+                success: false,
+                message: 'Email already exist!',
+            })
     }
 
-    if(req.body?.image) {
+    if (req.body?.image) {
 
     }
 })
@@ -76,3 +83,138 @@ export const adminSignout = asyncHandler(async (req, res) => {
         message: 'Logout successful!'
     })
 })
+
+
+/**
+ * @desc Fetch user's data
+ * @method GET api/admin/users
+ * @access Private
+ */
+export const fetchUsers = asyncHandler(async (req, res) => {
+    const usersData = await User.find({ isAdmin: { $eq: false } })
+        .select('-password')
+        .sort({ updatedAt: -1 });
+    res.status(200).json({
+        success: true,
+        error: false,
+        data: usersData,
+        message: 'users data fetch successfully!'
+    })
+})
+
+
+/**
+ * @desc update user data
+ * @method PUT api/admin/user:id
+ * @access Private
+ */
+export const updateUser = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const { name, email, profileImg } = req.body;
+    try {
+        const user = await User.findById(id);
+
+        if (!user) {
+            return res.status(404)
+                .json({
+                    error: true,
+                    success: false,
+                    message: 'User not found',
+
+                })
+        }
+        const isUserExist = await User.findOne({ name, _id: { $ne: id } });
+        const isEmailExist = await User.findOne({ email, _id: { $ne: id } });
+
+        if (isUserExist) {
+            return res.status(400)
+                .json({
+                    success: false,
+                    error: true,
+                    message: "Username already exist!"
+                })
+        }
+        if (isEmailExist) {
+            return res.status(400)
+                .json({
+                    success: false,
+                    error: true,
+                    message: "Email already exist!"
+                })
+        }
+        if (profileImg) {
+            const response = await cloudinary.uploader.upload(profileImg, {
+
+            })
+            user.profileImg = response.secure_url;
+        }
+        user.name = name || user.name;
+        user.email = email || user.email;
+
+        const updatedUser = await User.save();
+
+        res.status(200)
+            .json({
+                error: false,
+                success: true,
+                message: "User detials updated successfully ",
+                data: {
+                    _id: updatedUser._id,
+                    name: updatedUser.name,
+                    email: updatedUser.email
+                }
+            })
+
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500)
+            .json({
+                error: true,
+                success: false,
+                message: "Something went wrong while updating user data"
+            })
+
+
+    }
+})
+
+
+/**
+ * @desc delet user
+ * @method DELETE api/admin/user:id
+ * @access Private
+ */
+export const deleteUser = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await User.findById(id);
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                error: true,
+                message: 'User not found!',
+            })
+        }
+        await User.findByIdAndDelete(id);
+        return res.status(200).json({
+            success: true,
+            error: false,
+            message: "User account has deleted!"
+        })
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500)
+        .json({
+            error:true,
+            success: false,
+            message: 'Something went wrong'
+        })
+        
+
+    }
+})
+
+
+
